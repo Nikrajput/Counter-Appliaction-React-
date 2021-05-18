f93097d370de8a7c4f76f51d7ddfcb2bd19fe19c
Nikrajput <nikrajput8383@gmail.com>
2021-05-19 01:03:46 +0530

    Pink

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ function App() {
       Counter Hooks
       <CounterHooks initialCount={0} />
       <button onClick={()=> setTheme(prevTheme=>{
-        return prevTheme === 'red' ? 'blue' : 'red'
+        return prevTheme === 'red' ? 'pink' : 'red'
       })}>Toggle Theme</button>
     </ThemeContext.Provider>
   )
